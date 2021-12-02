3eb680b009b702752046e2d7d0226afdd4dc68fb
Lucas <lucas3couto@gmail.com>
2021-12-02 18:12:00 -0300

    styles: edit base style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,10 @@ import { Layout } from 'components/common'
 function HomePage() {
   return (
     <Layout>
-      <div className="bg-purple-500">Welcome to Next.js!</div>
+      <div className="max-w-5xl px-8 py-4 mx-auto">
+        <h1>Welcome to Next Boilerplate!</h1>
+        <p>Um boilerplate de fácil inicialização</p>
+      </div>
     </Layout>
   )
 }
